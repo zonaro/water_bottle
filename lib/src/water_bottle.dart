d098acfe34d5c181bfee7488078e2a6e092f85cb
Zonaro <zonaro@outlook.com>
2024-09-13 13:22:48 -0300

    Update homepage URL in pubspec.yaml fix background color of bottle

--- lib/src/water_bottle.dart
+++ lib/src/water_bottle.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart';
 import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+import 'bubble.dart';
 import 'water_container.dart';
 import 'wave.dart';
-import 'bubble.dart';
 
 class WaterBottle extends StatefulWidget {
   /// Color of the water
@@ -16,54 +18,9 @@ class WaterBottle extends StatefulWidget {
 
   /// Create a regular bottle, you can customize it's part with
   /// [waterColor], [bottleColor], [capColor].
-  WaterBottle(
-      {Key? key,
-      this.waterColor = Colors.blue,
-      this.bottleColor = Colors.blue,
-      this.capColor = Colors.blueGrey})
-      : super(key: key);
+  WaterBottle({Key? key, this.waterColor = Colors.blue, this.bottleColor = Colors.blue, this.capColor = Colors.blueGrey}) : super(key: key);
   @override
   WaterBottleState createState() => WaterBottleState();
-}
-
-class WaterBottleState extends State<WaterBottle>
-    with TickerProviderStateMixin, WaterContainer {
-  @override
-  void initState() {
-    super.initState();
-    initWater(widget.waterColor, this);
-    waves.first.animation.addListener(() {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    disposeWater();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      clipBehavior: Clip.hardEdge,
-      children: [
-        AspectRatio(
-          aspectRatio: 1 / 1,
-          child: CustomPaint(
-            painter: WaterBottlePainter(
-              waves: waves,
-              bubbles: bubbles,
-              waterLevel: waterLevel,
-              bottleColor: widget.bottleColor,
-              capColor: widget.capColor,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
 }
 
 class WaterBottlePainter extends CustomPainter {
@@ -82,14 +39,7 @@ class WaterBottlePainter extends CustomPainter {
   /// Bottle cap color
   final capColor;
 
-  WaterBottlePainter(
-      {Listenable? repaint,
-      required this.waves,
-      required this.bubbles,
-      required this.waterLevel,
-      required this.bottleColor,
-      required this.capColor})
-      : super(repaint: repaint);
+  WaterBottlePainter({Listenable? repaint, required this.waves, required this.bubbles, required this.waterLevel, required this.bottleColor, required this.capColor}) : super(repaint: repaint);
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -102,7 +52,7 @@ class WaterBottlePainter extends CustomPainter {
     }
     {
       final paint = Paint();
-      paint.color = Colors.white;
+      paint.color = bottleColor;
       paint.style = PaintingStyle.fill;
       final rect = Rect.fromLTRB(0, 0, size.width, size.height);
       canvas.saveLayer(rect, paint);
@@ -136,89 +86,19 @@ class WaterBottlePainter extends CustomPainter {
     }
   }
 
-  void paintEmptyBottle(Canvas canvas, Size size, Paint paint) {
-    final neckTop = size.width * 0.1;
-    final neckBottom = size.height;
-    final neckRingOuter = 0.0;
-    final neckRingOuterR = size.width - neckRingOuter;
-    final neckRingInner = size.width * 0.1;
-    final neckRingInnerR = size.width - neckRingInner;
-    final path = Path();
-    path.moveTo(neckRingOuter, neckTop);
-    path.lineTo(neckRingInner, neckTop);
-    path.lineTo(neckRingInner, neckBottom);
-    path.lineTo(neckRingInnerR, neckBottom);
-    path.lineTo(neckRingInnerR, neckTop);
-    path.lineTo(neckRingOuterR, neckTop);
-    canvas.drawPath(path, paint);
-  }
-
   void paintBottleMask(Canvas canvas, Size size, Paint paint) {
     final neckRingInner = size.width * 0.1;
     final neckRingInnerR = size.width - neckRingInner;
-    canvas.drawRect(
-        Rect.fromLTRB(
-            neckRingInner + 5, 0, neckRingInnerR - 5, size.height - 5),
-        paint);
-  }
-
-  void paintWaves(Canvas canvas, Size size, Paint paint) {
-    for (var wave in waves) {
-      paint.color = wave.color;
-      final transform = Matrix4.identity();
-      final desiredW = 15 * size.width;
-      final desiredH = 0.1 * size.height;
-      final translateRange = desiredW - size.width;
-      final scaleX = desiredW / wave.svgData.getBounds().width;
-      final scaleY = desiredH / wave.svgData.getBounds().height;
-      final translateX = -wave.offset * translateRange;
-      final waterRange = size.height +
-          desiredH; // 0 = no water = size.height; 1 = full water = -size.width
-      final translateY = (1.0 - waterLevel) * waterRange - desiredH;
-      transform.translate(translateX, translateY);
-      transform.scale(scaleX, scaleY);
-      canvas.drawPath(wave.svgData.transform(transform.storage), paint);
-      if (waves.indexOf(wave) != waves.length - 1) {
-        continue;
-      }
-      final gap = size.height - desiredH - translateY;
-      if (gap > 0) {
-        canvas.drawRect(
-            Rect.fromLTRB(0, desiredH + translateY, size.width, size.height),
-            paint);
-      }
-    }
+    canvas.drawRect(Rect.fromLTRB(neckRingInner + 5, 0, neckRingInnerR - 5, size.height - 5), paint);
   }
 
   void paintBubbles(Canvas canvas, Size size, Paint paint) {
     for (var bubble in bubbles) {
       paint.color = bubble.color;
-      final offset = Offset(
-          bubble.x * size.width, (bubble.y + 1.0 - waterLevel) * size.height);
+      final offset = Offset(bubble.x * size.width, (bubble.y + 1.0 - waterLevel) * size.height);
       final radius = bubble.size * math.min(size.width, size.height);
       canvas.drawCircle(offset, radius, paint);
     }
-  }
-
-  void paintGlossyOverlay(Canvas canvas, Size size, Paint paint) {
-    paint.color = Colors.white.withAlpha(20);
-    canvas.drawRect(Rect.fromLTRB(0, 0, size.width * 0.5, size.height), paint);
-    paint.color = Colors.white.withAlpha(80);
-    canvas.drawRect(
-        Rect.fromLTRB(size.width * 0.9, 0, size.width * 0.95, size.height),
-        paint);
-    final rect = Offset.zero & size;
-    final gradient = LinearGradient(
-      begin: Alignment.topLeft,
-      end: Alignment.topRight,
-      colors: [
-        Colors.white.withAlpha(180),
-        Colors.white.withAlpha(0),
-      ],
-    ).createShader(rect);
-    paint.color = Colors.white;
-    paint.shader = gradient;
-    canvas.drawRect(Rect.fromLTRB(0, 0, size.width, size.height), paint);
   }
 
   void paintCap(Canvas canvas, Size size, Paint paint) {
@@ -240,6 +120,106 @@ class WaterBottlePainter extends CustomPainter {
     canvas.drawPath(path, paint);
   }
 
+  void paintEmptyBottle(Canvas canvas, Size size, Paint paint) {
+    final neckTop = size.width * 0.1;
+    final neckBottom = size.height;
+    final neckRingOuter = 0.0;
+    final neckRingOuterR = size.width - neckRingOuter;
+    final neckRingInner = size.width * 0.1;
+    final neckRingInnerR = size.width - neckRingInner;
+    final path = Path();
+    path.moveTo(neckRingOuter, neckTop);
+    path.lineTo(neckRingInner, neckTop);
+    path.lineTo(neckRingInner, neckBottom);
+    path.lineTo(neckRingInnerR, neckBottom);
+    path.lineTo(neckRingInnerR, neckTop);
+    path.lineTo(neckRingOuterR, neckTop);
+    canvas.drawPath(path, paint);
+  }
+
+  void paintGlossyOverlay(Canvas canvas, Size size, Paint paint) {
+    paint.color = Colors.white.withAlpha(20);
+    canvas.drawRect(Rect.fromLTRB(0, 0, size.width * 0.5, size.height), paint);
+    paint.color = Colors.white.withAlpha(80);
+    canvas.drawRect(Rect.fromLTRB(size.width * 0.9, 0, size.width * 0.95, size.height), paint);
+    final rect = Offset.zero & size;
+    final gradient = LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.topRight,
+      colors: [
+        Colors.white.withAlpha(180),
+        Colors.white.withAlpha(0),
+      ],
+    ).createShader(rect);
+    paint.color = Colors.white;
+    paint.shader = gradient;
+    canvas.drawRect(Rect.fromLTRB(0, 0, size.width, size.height), paint);
+  }
+
+  void paintWaves(Canvas canvas, Size size, Paint paint) {
+    for (var wave in waves) {
+      paint.color = wave.color;
+      final transform = Matrix4.identity();
+      final desiredW = 15 * size.width;
+      final desiredH = 0.1 * size.height;
+      final translateRange = desiredW - size.width;
+      final scaleX = desiredW / wave.svgData.getBounds().width;
+      final scaleY = desiredH / wave.svgData.getBounds().height;
+      final translateX = -wave.offset * translateRange;
+      final waterRange = size.height + desiredH; // 0 = no water = size.height; 1 = full water = -size.width
+      final translateY = (1.0 - waterLevel) * waterRange - desiredH;
+      transform.translate(translateX, translateY);
+      transform.scale(scaleX, scaleY);
+      canvas.drawPath(wave.svgData.transform(transform.storage), paint);
+      if (waves.indexOf(wave) != waves.length - 1) {
+        continue;
+      }
+      final gap = size.height - desiredH - translateY;
+      if (gap > 0) {
+        canvas.drawRect(Rect.fromLTRB(0, desiredH + translateY, size.width, size.height), paint);
+      }
+    }
+  }
+
   @override
   bool shouldRepaint(WaterBottlePainter oldDelegate) => true;
+}
+
+class WaterBottleState extends State<WaterBottle> with TickerProviderStateMixin, WaterContainer {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      clipBehavior: Clip.hardEdge,
+      children: [
+        AspectRatio(
+          aspectRatio: 1 / 1,
+          child: CustomPaint(
+            painter: WaterBottlePainter(
+              waves: waves,
+              bubbles: bubbles,
+              waterLevel: waterLevel,
+              bottleColor: widget.bottleColor,
+              capColor: widget.capColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    disposeWater();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initWater(widget.waterColor, this);
+    waves.first.animation.addListener(() {
+      setState(() {});
+    });
+  }
 }
